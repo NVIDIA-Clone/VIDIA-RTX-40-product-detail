f9f6b30372a68dfc1fa60dd4756b3fa0ac2743fa
joelaspe <joe.laspe@gmail.com>
2023-07-21 15:52:38 -0700

    Saving for merge with innerNavbar skeleton

--- vite-project/src/components/OuterNavBar.jsx
+++ vite-project/src/components/OuterNavBar.jsx
@@ -1,6 +1,6 @@
 const OuterNavBar = () => {
     return (
-        <nav className="h-[44px] w-full bg-NVBGGray text-NVWhiteTxt font-Sig flex flex-row" >
+        <nav className="h-[66px] w-full bg-black text-NVWhiteTxt font-Sig flex flex-row" >
             <div className="sub-brand-nav-container flex">
                 GeForce
                 <ul className="flex">
@@ -11,7 +11,7 @@ const OuterNavBar = () => {
                           <path fillRule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clipRule="evenodd" />
                         </svg>
                       </button>
-                  
+                      
                       
                       <ul className="flex flex-col">
                             <li>Graphics Cards & Desktops</li>
