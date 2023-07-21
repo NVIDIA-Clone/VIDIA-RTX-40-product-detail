c7e5ea9ca3a831fea607f2588f1a0f34a841d643
BenjaminSchenk <benjamin.k.schenk@gmail.com>
2023-07-21 15:42:04 -0700

    inner nav

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,5 +1,9 @@
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Header from './components/Header';
+import LandingPage from './components/LandingPage';
+import OuterNavBar from './components/OuterNavBar';
+import { LandingPageProvider } from './components/LandingPageContext';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
