0409ae9251c228a79570ca5d5e262822b71517c3
joelaspe <joe.laspe@gmail.com>
2023-07-21 15:23:03 -0700

    Added button to first menu

--- vite-project/src/components/OuterNavBar.jsx
+++ vite-project/src/components/OuterNavBar.jsx
@@ -11,14 +11,20 @@ const OuterNavBar = () => {
                           <path fillRule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clipRule="evenodd" />
                         </svg>
                       </button>
-                        <ul className="flex flex-col">
+                      <div class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
+                      <div class="py-1" role="none">
+                      <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-0">Account settings</a>
+                      <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-1">Support</a>
+                      <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-2">License</a>
+                      
+                      <ul className="flex flex-col">
                             <li>Graphics Cards & Desktops</li>
                             <li>Laptops</li>
                             <li>G-SYNC Monitors</li>
-                            <li>GeForce NOW Cloud Gaming</li>
+                            <li>PeForce NOW Cloud Gaming</li>
                         </ul>
                     <li className="sub-brand-nav-item">Games & Tech</li>
-                    <li className="sub-brand-nav-item">GeForce Experience</li>
+                    <li className="sub-brand-nav-item">PeForce Experience</li>
                     <li className="sub-brand-nav-item">Drivers</li>
                     <li className="sub-brand-nav-item">News</li>
                     <li className="sub-brand-nav-item">Support</li>
