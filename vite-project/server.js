3da22ad13d432bd104a00440d86fcb6cad26ff3b
Bao Tran <Baottran21@outlook.com>
2023-07-21 16:01:39 -0500

    Able to retrieve data from the Render API

--- vite-project/server.js
+++ vite-project/server.js
@@ -1,18 +1,19 @@
 /* eslint-disable no-undef */
 import express from 'express';
 import pg from 'pg';
+import dotenv from 'dotenv';
+import cors from 'cors';
 const { Pool } = pg;
 const app = express();
-import dotenv from 'dotenv';
 dotenv.config();
 const PORT = process.env.PORT;
-
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
 app.use(express.static('dist'));
 app.use(express.json());
+app.use(cors({ origin: '*' }));
 
 app.get('/product', async (req, res) => {
   try {
