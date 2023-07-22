315a500b76e4130faf24883563ffb09e72402472
joelaspe <joe.laspe@gmail.com>
2023-07-21 21:34:57 -0700

    Added new gray color used in NVDIA navbars.

--- vite-project/src/components/OuterNavBar.jsx
+++ vite-project/src/components/OuterNavBar.jsx
@@ -1,12 +1,12 @@
 const OuterNavBar = () => {
     return (
-        <nav className="h-[66px] w-full bg-black text-NVWhiteTxt font-Sig flex flex-row items-center" >
+        <nav className="h-[66px] w-full bg-black text-NVNavBarGray font-Sig flex flex-row items-center" >
             <div className="sub-brand-nav-container py-0 px-8 flex">
-                <span className="sub-brand-name font-semibold text-2xl ">PeForce</span>
+                <span className="sub-brand-name text-NVWhiteTxt font-semibold text-2xl ">PeForce</span>
                 <ul className="flex">
                     <li className="sub-brand-nav-item"></li>
                         <div className="relative inline-block text-left">
-                            <span type="button" id="dropDownHoverBtnProducts" className="inline-flex w-full justify-center gap-x-1.5 pl-3 px-3 py-2 text-sm tex-gray-900 hover:bg-gray-50" aria-expanded="true" aria-haspopup="true" data-dropdown-toggle="dropDownHoverProducts" data-dropdown-trigger="hover">Products</span>
+                            <span type="button" id="dropDownHoverBtnProducts" className="inline-flex w-full justify-center gap-x-1.5 pl-3 px-3 py-2 text-sm text-gray-400 hover:text-white" aria-expanded="true" aria-haspopup="true" data-dropdown-toggle="dropDownHoverProducts" data-dropdown-trigger="hover">Products</span>
                             <div id="dropDownHoverProducts" className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabIndex="-1">
                             <div className="py-1" role="none">
                                 <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="menu-item-0">Graphics Cards & Desktops</a>
