43dacbaec6ae6e8e149e5c18d65d9676e9fe7589
joelaspe <joe.laspe@gmail.com>
2023-07-21 21:24:12 -0700

    Padding and adjustment of navbar container. Removed button and changed to a span. Orientation is better now.

--- vite-project/src/components/OuterNavBar.jsx
+++ vite-project/src/components/OuterNavBar.jsx
@@ -1,24 +1,25 @@
 const OuterNavBar = () => {
     return (
-        <nav className="h-[66px] w-full bg-black text-NVWhiteTxt font-Sig flex flex-row" >
-            <div className="sub-brand-nav-container flex">
-                GeForce
+        <nav className="h-[66px] w-full bg-black text-NVWhiteTxt font-Sig flex flex-row items-center" >
+            <div className="sub-brand-nav-container py-0 px-8 flex">
+                <span className="sub-brand-name font-semibold text-2xl ">PeForce</span>
                 <ul className="flex">
-                    <li className="sub-brand-nav-item">Products</li>
-                        <button type="button" className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold tex-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50" id="menu-button" aria-expanded="true" aria-haspopup="true">
-                        Options
-                        <svg className="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                          <path fillRule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clipRule="evenodd" />
-                        </svg>
-                      </button>
-                      
-                      
-                      <ul className="flex flex-col">
-                            <li>Graphics Cards & Desktops</li>
-                            <li>Laptops</li>
-                            <li>G-SYNC Monitors</li>
-                            <li>PeForce NOW Cloud Gaming</li>
-                        </ul>
+                    <li className="sub-brand-nav-item"></li>
+                        <div className="relative inline-block text-left">
+                            <span type="button" id="dropDownHoverBtnProducts" className="inline-flex w-full justify-center gap-x-1.5 pl-3 px-3 py-2 text-sm tex-gray-900 hover:bg-gray-50" aria-expanded="true" aria-haspopup="true" data-dropdown-toggle="dropDownHoverProducts" data-dropdown-trigger="hover">Products</span>
+                            <div id="dropDownHoverProducts" className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabIndex="-1">
+                            <div className="py-1" role="none">
+                                <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="menu-item-0">Graphics Cards & Desktops</a>
+                                <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="menu-item-1">Laptops</a>
+                                <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="menu-item-2">G-Sync Monitors</a>
+                                <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="menu-item-0">PeForce NOW CLoud Gaming</a>
+                                <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="menu-item-1">Support</a>
+                                <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="menu-item-2">License</a>
+                                
+                            </div>
+                        </div>
+                    </div>
+           
                     <li className="sub-brand-nav-item">Games & Tech</li>
                     <li className="sub-brand-nav-item">PeForce Experience</li>
                     <li className="sub-brand-nav-item">Drivers</li>
