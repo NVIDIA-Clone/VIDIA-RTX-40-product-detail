65388a133b89b88ee9b3f49870f326dfb0694e04
Autumn Sheridan <animalsoa@gmail.com>
2023-07-24 12:46:23 -0700

    a commit

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
 import Header from "./components/Header";
 import LandingPage from "./components/LandingPage";
 import OuterNavBar from "./components/OuterNavBar";
 import { LandingPageProvider } from "./components/LandingPageContext";
 import Footer from "./components/Footer";
-import InnerNavbar from "./components/InnerNavbar";
-=======
-import Header from './components/Header';
-import LandingPage from './components/LandingPage';
-import OuterNavBar from './components/OuterNavBar';
-import { LandingPageProvider } from './components/LandingPageContext';
-import Footer from './components/Footer';
-import InnerNavbar from './components/inner-navbar';
->>>>>>> 5625116a245358e03a4f1b6da78a87c1d1448066
+import InnerNavbar from "./components/inner-navbar";
 
 function App() {
   return (
