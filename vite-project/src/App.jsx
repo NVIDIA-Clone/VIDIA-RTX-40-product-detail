9a8d26cd2b5598e635fac93e6a01b2f9c000b832
joelaspe <joe.laspe@gmail.com>
2023-07-24 12:48:51 -0700

    merge commit

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,17 +1,11 @@
-<<<<<<< HEAD
+
 import Header from "./components/Header";
 import LandingPage from "./components/LandingPage";
 import OuterNavBar from "./components/OuterNavBar";
 import { LandingPageProvider } from "./components/LandingPageContext";
 import Footer from "./components/Footer";
 import InnerNavbar from "./components/InnerNavbar";
-=======
-import Header from './components/Header';
-import LandingPage from './components/LandingPage';
-import OuterNavBar from './components/OuterNavBar';
-import { LandingPageProvider } from './components/LandingPageContext';
-import Footer from './components/Footer';
-import InnerNavbar from './components/inner-navbar';
+
 
 
 function App() {
