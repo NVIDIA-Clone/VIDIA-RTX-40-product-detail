d5dc086e7fcb0b05dfbcc1c5708cd75523000ec0
Nathanvititoe <nathanvititoe@gmail.com>
2023-07-24 15:53:50 -0700

    built out performance div and styled

--- vite-project/tailwind.config.js
+++ vite-project/tailwind.config.js
@@ -7,13 +7,15 @@ export default {
         Sig: ["Signika", "sans-serif"],
       },
       colors: {
-        NVGreen: "#5e9400",
+        NVGreen: "#76B900",
         NVBGGray: "#1A1A1A",
         BHGreen: "#749f29",
         NVWhiteTxt: "#e8e6e3",
+        NVDLightGray: "#999999"
       },
     },
     screens: {
+      'md' : {'max' : '780px'},
       'sm' : {'max' : '640px'},
       'xs' : {'max' : '485px'},
       'xxs' : {'max' : '365px'}
