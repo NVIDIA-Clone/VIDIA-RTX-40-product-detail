a831aff5048d5d93ff22dc02430ee254259bdd53
joelaspe <joe.laspe@gmail.com>
2023-07-25 09:04:00 -0700

    Fixed alignment of navbar items

--- vite-project/src/components/OuterNavBar/OuterNavBar.jsx
+++ vite-project/src/components/OuterNavBar/OuterNavBar.jsx
@@ -8,23 +8,28 @@ const OuterNavBar = () => {
         <nav className="h-[66px] w-full bg-black text-NVNavBarGray font-Sig hover:cursor-pointer flex flex-row items-center" >
             <div className="sub-brand-nav-container py-0 px-8 flex">
                 <span className="sub-brand-name text-NVWhiteTxt font-semibold text-2xl ">PeForce</span>
-                <ul className="flex">
+                <ul className="flex items-center">
                     <li className="sub-brand-nav-item relative dropdown"></li>
                         <div className="relative inline-block text-left text-base">
                             <ProductDropdown /> 
-                            
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                                 <path d="M7 10l5 5 5-5z"></path>
+                                 <path d="M0 0h24v24H0z" fill="none"></path>
+                            </svg>
                         </div>
                     <li className="sub-brand-nav-item">
                         <div className="relative inline-block text-left text-base">
                             <GamesDropdown />
                         </div>
                     </li>
-                    <li className="sub-brand-nav-item">PeForce Experience</li>
-                    <li className="sub-brand-nav-item">Drivers</li>
-                    <li className="sub-brand-nav-item">News</li>
-                    <li className="sub-brand-nav-item">Support</li>
-                    <li className="sub-brand-nav-item">Shop</li>
-                    <li className="sub-brand-nav-item">Test</li>
+                    <li className="sub-brand-nav-item hover:text-NVGreen tracking-wide">
+                        <div className="relative inline-block text-left text-base">PeForce Experience</div>
+                    </li>
+                    <li className="sub-brand-nav-item hover:text-NVGreen tracking-wide">
+                        <div className="relative inline-block text-left text-base">Drivers</div>
+                    </li>
+                    <li className="sub-brand-nav-item hover:text-NVGreen">News</li>
+                    <li className="sub-brand-nav-item tracking-wide">Support&Shop</li>
                 </ul>
             </div>
         </nav>
