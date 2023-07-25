ff5219f6c3a479bec819d4a6a0de2eccb1bd24d4
Autumn Sheridan <animalsoa@gmail.com>
2023-07-25 07:25:20 -0700

    a commit

--- vite-project/src/components/RayTracing.jsx
+++ vite-project/src/components/RayTracing.jsx
@@ -2,7 +2,7 @@ const RayTracing = () => {
   return (
     <>
       <div className="float-left w-[100%] ">
-        <div className="bg-black pt-75px pb-75px w-[1588px] h-[392.1px]">
+        <div className="bg-black pt-75px pb-75px h-[392.1px]">
           <div className="max-w-[990pt] ml-auto mr-auto pl-0 pr-0 w-[100%] z-[1] ">
             <div className="block w-[100%]">
               <div className="float-left w-[100%] pt-[8px] pr-[15px] pb-[8px] pl-[15px]"></div>
