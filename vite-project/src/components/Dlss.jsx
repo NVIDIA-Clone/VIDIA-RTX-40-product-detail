e79a579f39b753961cf0f3d97ede8e6e4780a424
joelaspe <joe.laspe@gmail.com>
2023-07-26 09:35:34 -0700

    Commiting for merge with main

--- vite-project/src/components/Dlss.jsx
+++ vite-project/src/components/Dlss.jsx
@@ -1,7 +1,7 @@
 const Dlss = () => {
     return (
 
-    <div className="grid grid-cols-8 grid-rows-6 gap-4 bg-local text-NVWhiteTxt bg-auto bg-[url('/images/nvidia-dlss-video-xlrg-hero-background-bb770-d.jpg')]">
+    <div className="grid grid-cols-8 grid-rows-6 gap-4 bg-local text-NVWhiteTxt bg-auto bg-no-repeat bg-[url('/images/nvidia-dlss-video-xlrg-hero-background-bb770-d.jpg')]">
         <div className="col-start-2 col-span-6 row-start-2 row-span-1 justify-self-center">
             <h2 className="text-lg text-center font-bold ">VIDIA DLSS 3</h2>
             <h1 className="text-3xl text-center font-bold">The Performance Multiplier, Powered by AI</h1>
