b1cb6479c461219a6c93e4bb4b204fbd0cd902ce
joelaspe <joe.laspe@gmail.com>
2023-07-26 13:10:59 -0700

    Added note below video

--- vite-project/src/components/Dlss.jsx
+++ vite-project/src/components/Dlss.jsx
@@ -1,7 +1,7 @@
 const Dlss = () => {
     return (
 
-    <div className="dlss grid grid-cols-8 grid-rows-[3] gap-14 bg-local text-NVWhiteTxt bg-auto bg-no-repeat bg-[url('/images/nvidia-dlss-video-xlrg-hero-background-bb770-d.jpg')]">
+    <div className="dlss grid grid-cols-8 grid-rows-[3] gap-14 bg-local font-Sig text-NVWhiteTxt bg-auto bg-no-repeat bg-[url('/images/nvidia-dlss-video-xlrg-hero-background-bb770-d.jpg')]">
         <div className="dlss-info col-start-2 col-span-6 row-start-2 row-span-1 justify-self-center">
             <h2 className="text-lg text-center font-bold ">VIDIA DLSS 3</h2>
             <h1 className="text-3xl text-center font-bold">The Performance Multiplier, Powered by AI</h1>
@@ -11,9 +11,11 @@ const Dlss = () => {
         <div className="video-embed col-start-2 col-span-6 row-start-3 row-span-1 max-w-screen-lg w-[850px] h-[500px] justify-self-center">
             <iframe className="relative top-0 w-full h-full" src="https://www.youtube.com/embed/JkQuGpJTHmU?origin=https%3A%2F%2Fwww.nvidia.com&amp;hl=en_US&amp;mute=1&amp;autoplay=0&amp;loop=0&amp;controls=1&amp;enablejsapi=1?origin=https%3A%2F%2Fwww.nvidia.com&amp;hl=en_US&amp;mute=1&amp;autoplay=0&amp;loop=0&amp;controls=1&amp;enablejsapi=0&amp;rel=0" allowfullscreen="" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" loading="lazy" aria-label="YouTube Video" data-modal-autoplay="false" frameBorder="0" >
             </iframe>
-         
             
         </div>
+        <div className="col-start-3 col-span-4 row-start-4 row-span-1 justify-self-center">
+            <p className="text-NVDLightGray text-center text-xs self-end">*Captured with GeForce RTX 4090 at 3840 x 2160</p>
+        </div> 
      </div> 
     )
 }
