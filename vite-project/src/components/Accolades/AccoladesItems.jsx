7b1c66537a3c2a76b3e3eb343c2bea21b2ca8fcd
Bao Tran <Baottran21@outlook.com>
2023-07-26 03:29:07 -0500

    Needing to change quote styles

--- vite-project/src/components/Accolades/AccoladesItems.jsx
+++ vite-project/src/components/Accolades/AccoladesItems.jsx
@@ -3,7 +3,7 @@ import Item from './Item';
 const AccoladesItems = () => {
   return (
     <>
-      <div id="accolades" className="font-Sig pt-[45px] pb-[15px]">
+      <div id="accolades" className="font-Sig pt-[45px] pb-[15px] delay-5000">
         <Item />
       </div>
     </>
