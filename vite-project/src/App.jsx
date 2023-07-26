c399a2db9896397fde693edd5c8dec54f5d333e7
Bao Tran <Baottran21@outlook.com>
2023-07-26 12:07:45 -0500

    Checking dev

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,15 +1,15 @@
-import Header from "./components/Header/Header";
-import LandingPage from "./components/LandingPage";
-import OuterNavBar from "./components/OuterNavBar/OuterNavBar";
-import { LandingPageProvider } from "./components/LandingPageContext";
-import Footer from "./components/Footer";
-import InnerNavbar from "./components/inner-navbar";
-import Architecture from "./components/Architecture";
-import Performance from "./components/Performance";
-import RayTracing from "./components/RayTracing/RayTracing";
-import Accolades from "./components/Accolades/Accolades";
-import Reflex from "./components/Reflex";
-import Dlss from "./components/Dlss";
+import Header from './components/Header/Header';
+import LandingPage from './components/LandingPage';
+import OuterNavBar from './components/OuterNavBar/OuterNavBar';
+import { LandingPageProvider } from './components/LandingPageContext';
+import Footer from './components/Footer';
+import InnerNavbar from './components/inner-navbar';
+import Architecture from './components/Architecture';
+import Performance from './components/Performance';
+import RayTracing from './components/RayTracing/RayTracing';
+import Accolades from './components/Accolades/Accolades';
+import Reflex from './components/Reflex';
+import Dlss from './components/Dlss';
 
 function App() {
   return (
@@ -23,6 +23,7 @@ function App() {
         <Architecture />
         <Performance />
         <RayTracing />
+        <Reflex />
         <Dlss />
         <Footer />
       </LandingPageProvider>
