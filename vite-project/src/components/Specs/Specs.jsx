c93cd2300dcb4f82f14e7094836d3b2286106ca6
joelaspe <joe.laspe@gmail.com>
2023-07-27 15:12:50 -0700

    Created modal button for Specs page. Initial Modal popup and functionality, with close arrow (non animated)

--- vite-project/src/components/Specs/Specs.jsx
+++ vite-project/src/components/Specs/Specs.jsx
@@ -1,37 +1,42 @@
-import { useContext } from "react";
-import LandingPageContext from "./LandingPageContext.jsx";
+import { useContext, useState } from "react";
+import LandingPageContext from "../LandingPageContext.jsx";
+import SpecModal from "./SpecModal";
 
 const Specs = () => {
 
     const { isHovered, handleMouseEnter, handleMouseLeave } = useContext(LandingPageContext);
-
+ 
     return (
         <div className="specs-grid grid grid-cols-8 grid-rows-6 bg-black font-Sig text-NVWhiteTxt w-screen">
-             <div className="specs-title col-start-3 col-span-4 row-start-1 row-span-1 justify-self-center w-28 text-2xl font-bold">
+             <div className="specs-title col-start-3 col-span-4 row-start-1 row-span-1 justify-self-center w-28 text-2xl py-12 font-bold">
                  <h1>Specs</h1>
             </div>
             <div className="spec-table col-start-3 col-span-4 row-start-2 row-span-2 justify-self-center">
                 <table className="table-fixed">
                     <tr className="specs-row border-t border-NVDropDownGray">
                         <td className="w-72 py-4">VIDIA CUDA Cores</td>
-                        <td className="w-40 py-4"><h5 className="text-lg font-bold">16384</h5></td>
+                        <td className="w-40 py-4"><h5 className="text-base font-bold">16384</h5></td>
                     </tr>
                     <tr className="specs-row border-t border-NVDropDownGray">
                         <td className="py-4">Boost Clock (GHz)</td>
-                        <td className="py-4"><h5 className="text-lg font-bold">2.52</h5></td>
+                        <td className="py-4"><h5 className="text-base font-bold">2.52</h5></td>
                     </tr>
                     <tr className="specs-row border-t border-NVDropDownGray">
                         <td className="py-4">Memory Size</td>
-                        <td className="py-4"><h5 className="text-lg font-bold">24 GB</h5></td>
+                        <td className="py-4"><h5 className="text-base font-bold">24 GB</h5></td>
                     </tr>
                     <tr className="specs-row border-t border-NVDropDownGray">
                         <td className="py-4">Memory Type</td>
-                        <td className="py-4"><h5 className="text-lg font-bold">GDDR6X</h5></td>
+                        <td className="py-4"><h5 className="text-base font-bold">GDDR6X</h5></td>
                     </tr>
                 </table>
             </div>
-            <div className="system-compatibility col-start-3 col-span4 row-start 3 row-span-1 justify-self-center">
-                <div className="text-center font-bold py-1 m-0.5" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+            <div className="full-specs-btn col-start-3 col-span-4 row-start-4 row-span-1 justify-self-center flex flex-col">
+                
+                <SpecModal />
+
+
+                <div className="text-center font-bold py-6 m-0.5" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
                 <span className="object-center">Check Your System Compatibility</span>
                 <button>
                     <svg
