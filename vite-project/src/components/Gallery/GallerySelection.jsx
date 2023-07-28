03499519025fab86db549aa84c1b57c81b7744ea
Bao Tran <Baottran21@outlook.com>
2023-07-28 18:40:21 -0500

    Pushing the most recent branch

--- vite-project/src/components/Gallery/GallerySelection.jsx
+++ vite-project/src/components/Gallery/GallerySelection.jsx
@@ -12,11 +12,15 @@ const GallerySelection = () => {
   //On Hover event
 
   return (
-    <>
-      <div className="relative opacity-50 bg-black z-0 bottom-7 h-[56px] flex items-center">
+    <div>
+      <div className="relative opacity-50 bg-black z-0 bottom-[55px] h-[56px] flex items-center w-full">
         <h1 className="text-[white] z-50 opacity-100 pl-[20px]">
           PeForce RTX 4090
         </h1>
+        <div>
+          <img src="/assets/fullScreenIcon.svg" alt="" />
+          <img src="/assets/galleryopen.svg" alt="" />
+        </div>
       </div>
       <div id="picture-carousel" className="w-full flex justify-center">
         {galleryPictures.map((elem, index) => (
@@ -35,7 +39,7 @@ const GallerySelection = () => {
           </div>
         ))}
       </div>
-    </>
+    </div>
   );
 };
 
