3f50c90af819bc2f6fc4a8d47bad29244ed6c655
Autumn Sheridan <animalsoa@gmail.com>
2023-07-28 10:42:53 -0700

    a commit

--- vite-project/src/components/Architecture/ArchitectureListItems.jsx
+++ vite-project/src/components/Architecture/ArchitectureListItems.jsx
@@ -5,35 +5,31 @@ import ArchitectureContext from "./ArchitectureContext";
 
 const ArchitectureListItems = () => {
   const { architectureList } = useContext(ArchitectureContext);
-  console.log(architectureList); 
   return (
     <div className="flex flex-wrap w-full justify-evenly over">
-      {architectureList.current.map(
-        (item, index) => (
-          console.log(item),
-          (
-            
-            <div key={index} className="flex flex-col items-center mb-[45px] z-50 w-[295px]">
-                <div className="mb-[24px]">
-              <svg width={64} height={64}>
-                <path
-                  id={item.architectureSymbol.id}
-                  d={item.architectureSymbol.d}
-                  fill={item.architectureSymbol.fill}
-                  ></path>
-              </svg>
-                  </div>
-              <p className="text-white py-[10px] mb-[8px] text-[1.5rem] font-bold text-center">
-                {item.title}
-                <br /> {item.subtitle}
-              </p>
-              <p className="text-white text-[1rem] mx-[17.5px] p-[10px] text-center">
-                {item.info}
-              </p>
-            </div>
-          )
-        )
-      )}
+      {architectureList.current.map((item, index) => (
+        <div
+          key={index}
+          className="flex flex-col items-center mb-[45px] z-50 w-[295px]"
+        >
+          <div className="mb-[24px]">
+            <svg width={64} height={64}>
+              <path
+                id={item.architectureSymbol.id}
+                d={item.architectureSymbol.d}
+                fill={item.architectureSymbol.fill}
+              ></path>
+            </svg>
+          </div>
+          <p className="text-white py-[10px] mb-[8px] text-[1.5rem] font-bold text-center">
+            {item.title}
+            <br /> {item.subtitle}
+          </p>
+          <p className="text-white text-[1rem] mx-[17.5px] p-[10px] text-center">
+            {item.info}
+          </p>
+        </div>
+      ))}
       ;
     </div>
   );
--- vite-project/src/components/Shop.jsx
+++ vite-project/src/components/Shop.jsx
@@ -1,7 +1,7 @@
 const Shop = () => {
   return (
     <>
-      <div className="bg-black float-left h-[1021.5px] w-full text-center">
+      <div className="bg-black float-left h-[500px] w-full text-center justify-center">
         <h1 className="text-white text-[36px] mt-[70px] font-bold">Shop</h1>
         <h3 className="text-white text-[36px] mt-[50px] font-bold">
           GeForce RTX 4090
@@ -9,6 +9,17 @@ const Shop = () => {
         <h2 className="text-white text-[26px] mt-[20px] font-bold">
           Starting at $1599.00
         </h2>
+        <button
+          className="text-[18px] mt-[20px] p-[13px_15px] bg-[#76b900] text-[black] font-bold text-base hover:bg-[#91c733]"
+          type="button"
+        >
+          See All Buying Options
+        </button>
+      </div>
+      <div className="bg-black float-left h-[600px] w-full text-center justify-center">
+        <h1 className="text-white text-[31px] mt-[70px] font-bold">
+          Also Available From
+        </h1>
       </div>
     </>
   );
