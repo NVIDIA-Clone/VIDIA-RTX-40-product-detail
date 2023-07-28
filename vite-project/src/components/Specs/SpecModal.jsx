b7df410a1e00940757ff4b493680c5503a92448f
joelaspe <joe.laspe@gmail.com>
2023-07-27 20:49:10 -0700

    Fixed alignment of modal close button, is now attached directly to grid

--- vite-project/src/components/Specs/SpecModal.jsx
+++ vite-project/src/components/Specs/SpecModal.jsx
@@ -1,7 +1,10 @@
-import {useState} from 'react';
+import {useState, useContext} from 'react';
+import LandingPageContext from "../LandingPageContext.jsx";
 
 const SpecModal = () => {
     const [showModal, setShowModal] = useState(false);
+    const { isHovered, handleMouseEnter, handleMouseLeave } = useContext(LandingPageContext);
+
     return (
         <>
         <button className="text-[18px] mt-2.5 ml-[15px] p-[13px_15px] bg-[#76b900] text-[black] font-bold text-base hover:bg-[#91c733]" type="button" onClick={() => setShowModal(true)}>View Full Specs
@@ -15,7 +18,16 @@ const SpecModal = () => {
                         onClick={() => setShowModal(false)}
                     ></div>
                     <div className="specs-modal-grid grid grid-cols-8 grid-rows-6 py-5 min-h-screen items-stretch" >
-                        <div className="full-specs-table col-start-3 col-span-4 row-start-1 row-span-1 relative w-full bg-[#262626] rounded-md shadow-lg justify-self-center">
+                        <div className="specs-modal-header col-start-3 col-span-4 row-start-1 row-span-1 flex items-start bg-[#262626] relative">
+                            <button type="button" className="text-gray-400 bg-transparent text-sm w-8 h-8 ml-auto inline-flex justify-center items-center" data-modal-hide="defaultModal">
+                                <span onClick={() => setShowModal(false) } onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="-3 2 24 24"><path className= "transition-transform duration-500 ease-in-out transform"  
+                                fill={isHovered ? "#A9A9A9" : "#6666"} d="M12,2A10,10,0,1,0,22,12,9.9909,9.9909,0,0,0,12,2Zm5,13.59L15.59,17,12,13.41,8.41,17,7,15.59,10.59,12,7,8.41,8.41,7,12,10.59,15.59,7,17,8.41,13.41,12Z" ></path><polygon className="nv-modal-close-cross" fill="#e8e6e3" points="7 8.41 8.41 7 12 10.59 15.59 7 17 8.41 13.41 12 17 15.59 15.59 17 12 13.41 8.41 17 7 15.59 10.59 12 7 8.41"></polygon></svg>
+                                </span>
+                            <span className="sr-only">Close modal</span>
+                            </button>
+                        </div>
+                        <div className="full-specs-table col-start-3 col-span-4 row-start-2 row-span-1 relative w-full bg-[#262626] rounded-md justify-self-center">
                                                          
                             <div className="mt-2 text-center sm:ml-4 sm:text-left">
                                 <p className="mt-2 text-[15px] leading-relaxed text-NVWhiteTxt">
@@ -23,10 +35,6 @@ const SpecModal = () => {
                                 </p>
                             </div>
                         </div>
-                        <div className="specs-modal-close-container col-start-7 col-span-1 row-start-1 relative w-full">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path className="nv-modal-close-oval" fill="#666" d="M12,2A10,10,0,1,0,22,12,9.9909,9.9909,0,0,0,12,2Zm5,13.59L15.59,17,12,13.41,8.41,17,7,15.59,10.59,12,7,8.41,8.41,7,12,10.59,15.59,7,17,8.41,13.41,12Z"></path><polygon className="nv-modal-close-cross" fill="#e8e6e3" points="7 8.41 8.41 7 12 10.59 15.59 7 17 8.41 13.41 12 17 15.59 15.59 17 12 13.41 8.41 17 7 15.59 10.59 12 7 8.41"></polygon></svg>
-                        </div>
-
                     </div>
                 </div>
             </>
@@ -37,9 +45,3 @@ const SpecModal = () => {
 
 export default SpecModal;
 
-/*
-<div className="specs-modal-close-container col-start-7 col-span-1 row-start-1 px-20 relative w-full max-w-lg mx-auto bg-white">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path className="nv-modal-close-oval" fill="#666" d="M12,2A10,10,0,1,0,22,12,9.9909,9.9909,0,0,0,12,2Zm5,13.59L15.59,17,12,13.41,8.41,17,7,15.59,10.59,12,7,8.41,8.41,7,12,10.59,15.59,7,17,8.41,13.41,12Z"></path><polygon className="nv-modal-close-cross" fill="#e8e6e3" points="7 8.41 8.41 7 12 10.59 15.59 7 17 8.41 13.41 12 17 15.59 15.59 17 12 13.41 8.41 17 7 15.59 10.59 12 7 8.41"></polygon></svg>
-                        </div>
-
-                        */
