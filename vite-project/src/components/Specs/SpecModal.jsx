872839f3d5563f71f01967190bd4be6c3e310d7c
joelaspe <joe.laspe@gmail.com>
2023-07-28 10:28:33 -0700

    Initial layout of table and specs data

--- vite-project/src/components/Specs/SpecModal.jsx
+++ vite-project/src/components/Specs/SpecModal.jsx
@@ -19,7 +19,7 @@ const SpecModal = () => {
                     ></div>
                     <div className="specs-modal-grid grid grid-cols-8 py-5 min-h-screen items-stretch" >
                         <div className="specs-modal-header col-start-2 col-span-6 row-start-1 row-span-1 flex items-start bg-[#1A1A1A] relative">
-                            <span className="padding div ml-auto inline-flex justify-center items-center px-4"></span>
+                            <span className="padding-div ml-auto inline-flex justify-center items-center px-4"></span>
                             <span className="text-3xl font-bold text-NVWhiteTxt py-16">PeForce RTX 4090</span>
                             <button type="button" className="text-gray-400 bg-transparent text-sm w-8 h-8 ml-auto inline-flex justify-center items-center" data-modal-hide="defaultModal">
                                 <span onClick={() => setShowModal(false) } onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
@@ -29,27 +29,71 @@ const SpecModal = () => {
                             <span className="sr-only">Close modal</span>
                             </button>
                         </div>
-                        <div className="specs-modal-table col-start-2 col-span-6 row-start-2 row-span-1 relative w-full bg-[#1A1A1A] rounded-md text-center justify-self-center">
-                        This will contain the table<br>
-                        </br>
-                        This will contain the table<br>
-                        </br>
-                        This will contain the table<br>
-                        </br>
-                        This will contain the table<br>
-                        </br>
-                        This will contain the table<br>
-                        </br>
-                        This will contain the table<br>
-                        </br>
-                        This will contain the table<br>
-                        </br>
-                        This will contain the table<br>
-                        </br>
-                        
-                        This will contain the table<br>
-                        </br>
-                                                  
+                        <div className="specs-modal-table col-start-2 col-span-6 row-start-2 row-span-1 relative w-full bg-[#1A1A1A] rounded-md text-center">
+                            <table className="table-fixed w-fit">
+                            <tbody>
+                            <tr className="specs-row">
+                                <td className="text-sm font-bold w-44 py-4">GPU Engine Specs:</td>
+                                <td className="w-40 py-4 text-left"><h5 className="text-sm">VIDIA CUDA Cores</h5></td>
+                                <td className="w-40 py-4"><h5 className="text-sm">{specs.vidia_cuda_cores}</h5></td>
+                            </tr>
+                            <tr className="specs-row ">
+                                <td className="text-sm font-bold py-4"></td>
+                                <td className="w-40 py-4 text-left border-t border-NVDropDownGray"><h5 className="text-sm">Boost Clock (GHz)</h5></td>
+                                <td className="w-40 py-4 border-t border-NVDropDownGray"><h5 className="text-sm">{specs.boost_clock}</h5></td>
+                            </tr>
+                            <tr className="specs-row ">
+                                <td className="text-sm font-bold py-4"></td>
+                                <td className="w-40 py-4 text-left border-t border-NVDropDownGray "><h5 className="text-sm">Base Clock (GHz)</h5></td>
+                                <td className="w-40 py-4 border-t border-NVDropDownGray"><h5 className="text-sm">{specs.base_clock}</h5></td>
+                            </tr>
+                            <tr className="specs-row ">
+                                <td className="text-sm font-bold py-4"></td>
+                                <td className="w-40 py-4 "></td>
+                                <td className="w-40 py-4"></td>
+                            </tr>
+                            <tr className="specs-row ">
+                                <td className="text-sm font-bold py-4">Memory Specs:</td>
+                                <td className="w-40 py-4 text-left"><h5 className="text-sm">Standard Memory Config</h5></td>
+                                <td className="w-40 py-4 "><h5 className="text-sm">{specs.standard_memory_config}</h5></td>
+                            </tr>
+                            <tr className="specs-row border-t border-NVDropDownGray">
+                                <td className="text-sm font-bold py-4"></td>
+                                <td className="w-40 py-4"><h5 className="text-sm">Memory Interface Width</h5></td>
+                                <td className="w-40 py-4"><h5 className="text-sm">{specs.memory_interface}</h5></td>
+                            </tr>
+                            <tr className="specs-row border-t border-NVDropDownGray">
+                                <td className="text-sm font-bold py-4">Technology Support:</td>
+                                <td className="w-40 py-4"><h5 className="text-sm">Ray Tracing Cores</h5></td>
+                                <td className="w-40 py-4"><h5 className="text-sm">{specs.ray_tracing_cores}</h5></td>
+                            </tr>
+                            <tr className="specs-row border-t border-NVDropDownGray">
+                                <td className="text-sm font-bold py-4"></td>
+                                <td className="w-40 py-4"><h5 className="text-sm">Tensor Cores</h5></td>
+                                <td className="w-40 py-4"><h5 className="text-sm">{specs.tensor_cores}</h5></td>
+                            </tr>
+                            <tr className="specs-row border-t border-NVDropDownGray">
+                                <td className="text-sm font-bold py-4"></td>
+                                <td className="w-40 py-4"><h5 className="text-sm">VIDIA Architecture</h5></td>
+                                <td className="w-40 py-4"><h5 className="text-sm">{specs.vidia_architecture}</h5></td>
+                            </tr>
+                            <tr className="specs-row border-t border-NVDropDownGray">
+                                <td className="text-sm font-bold py-4"></td>
+                                <td className="w-40 py-4"><h5 className="text-sm">VIDIA DLSS</h5></td>
+                                <td className="w-40 py-4"><h5 className="text-sm">{specs.vidia_dlss}</h5></td>
+                            </tr>
+                            <tr className="specs-row border-t border-NVDropDownGray">
+                                <td className="text-sm font-bold py-4"></td>
+                                <td className="w-40 py-4"><h5 className="text-sm">VIDIA Reflex</h5></td>
+                                <td className="w-40 py-4"><h5 className="text-sm">{specs.vidia_reflex ? 'Yes' : 'No'}</h5></td>
+                            </tr>
+                            <tr className="specs-row border-t border-NVDropDownGray">
+                                <td className="text-sm font-bold py-4"></td>
+                                <td className="w-40 py-4"><h5 className="text-sm">VIDIA Broadcast</h5></td>
+                                <td className="w-40 py-4"><h5 className="text-sm">{specs.vidia_broadcast ? 'Yes' : 'No'}</h5></td>
+                            </tr>
+                            </tbody>
+                        </table>                                       
                             
                         </div>
                     </div>
