cd3b6be090c1d008199728f516969ac93f2a4433
BenjaminSchenk <benjamin.k.schenk@gmail.com>
2023-07-30 15:04:23 -0700

    Fixed some css

--- vite-project/src/components/InnerNavbar.jsx
+++ vite-project/src/components/InnerNavbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, } from 'react';
 
 const InnerNavbar = () => {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
