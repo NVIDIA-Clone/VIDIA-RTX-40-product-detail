e983d6110aea2bb37bc8edb6fdfa9f6250ca6fb4
Autumn Sheridan <animalsoa@gmail.com>
2023-08-03 12:33:38 -0700

    added Docker

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true,
+    port: 9000, // This is the port which we will use in docker
+    // add the next lines if you're using windows and hot reload doesn't work
+    watch: {
+      usePolling: true,
+    },
+  },
+});
