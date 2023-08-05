b263059ccb96f467dd3de5ec611be530be9d9f64
Joseph Carrillo <jlcarrillojr81@gmail.com>
2023-08-05 11:02:52 -1000

    Updates for database connection

--- vite-project/psqlServer.js
+++ vite-project/psqlServer.js
@@ -1,19 +1,19 @@
-/* eslint-disable no-undef */
-import express from 'express';
-import pg from 'pg';
-import dotenv from 'dotenv';
-import cors from 'cors';
-const { Pool } = pg;
-const app = express();
-dotenv.config();
-const PORT = process.env.PORT;
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
+// /* eslint-disable no-undef */
+// import express from 'express';
+// import pg from 'pg';
+// import dotenv from 'dotenv';
+// import cors from 'cors';
+// const { Pool } = pg;
+// const app = express();
+// dotenv.config();
+// const PORT = process.env.PORT;
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+// });
 
-app.use(express.static('dist'));
-app.use(express.json());
-app.use(cors({ origin: '*' }));
+// app.use(express.static('dist'));
+// app.use(express.json());
+// app.use(cors({ origin: '*' }));
 
 // app.get('/product', async (req, res) => {
 //   try {
@@ -25,24 +25,24 @@ app.use(cors({ origin: '*' }));
 //   }
 // });
 
-app.get('/product/:id', async (req, res) => {
-  const { id } = req.params;
-  // check if input is a valid integer
-  if(isNaN(parseInt(id, 10))) {
-    res.status(400).send("Bad Request: The request cannot be fulfilled due to bad syntax.");
-    return;
-  }
-  try {
-    const result = await pool.query('SELECT * FROM specs WHERE spec_id = $1', [
-      id,
-    ]);
-    res.status(200).send(result.rows[0]);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Internal server error.');
-  }
-});
+// app.get('/product/:id', async (req, res) => {
+//   const { id } = req.params;
+//   // check if input is a valid integer
+//   if(isNaN(parseInt(id, 10))) {
+//     res.status(400).send("Bad Request: The request cannot be fulfilled due to bad syntax.");
+//     return;
+//   }
+//   try {
+//     const result = await pool.query('SELECT * FROM specs WHERE spec_id = $1', [
+//       id,
+//     ]);
+//     res.status(200).send(result.rows[0]);
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).send('Internal server error.');
+//   }
+// });
 
-app.listen(PORT, () => {
-  console.log('Listening on port', PORT);
-});
+// app.listen(PORT, () => {
+//   console.log('Listening on port', PORT);
+// });
