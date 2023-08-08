5fd950caed781e27689164b488dec19a151145b9
Garett-Lubecki <garett934@gmail.com>
2023-08-08 15:28:09 -0400

    cors thing

--- vite-project/mongoServer.js
+++ vite-project/mongoServer.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.static("dist"));
 app.use(express.json());
 app.use(cors({
-  origin: ['http://localhost:5175']
+  origin: "*"
 }));
 
 if (cluster.isMaster) {
